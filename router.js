0a931a51de56e728285df26a75442e0e556ae47c
Dileep-krishna <dileepkrishna7178@gmail.com>
2026-05-27 00:15:15 +0530

    fix controller path for render deployment

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const  express=require("express")
 const multer = require("multer");
 
 const multerConfig = require("./middlewares/multerConfig");
-const { addDeliveryBoy, getAllDeliveryBoys, deleteDeliveryBoy, updateDeliveryBoy,  } = require("./controller/DeliveryboysController");
+const { addDeliveryBoy, getAllDeliveryBoys, deleteDeliveryBoy, updateDeliveryBoy,  } = require("./controllers/DeliveryboysController");
 
 
 
